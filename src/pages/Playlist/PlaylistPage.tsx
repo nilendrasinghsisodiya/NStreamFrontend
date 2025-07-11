806cd1b32b0a09b16c265d755e8b5bca4e42723c
nilendrasinghsisodiya <niendrasinghsisodiya@gmail.com>
2025-07-11 22:41:52 +0530

    fix: fixed virual list disapperaing problem

--- src/pages/Playlist/PlaylistPage.tsx
+++ src/pages/Playlist/PlaylistPage.tsx
@@ -1,6 +1,6 @@
 import { VirtuosoGrid, GridComponents } from "react-virtuoso";
 import { useGetPlaylist } from "@/api/PlaylistApi";
-import {  VideoCard } from "@/components/video/VideoCard";
+import {  VideoCardPlaylist } from "@/components/video/VideoCard";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 
@@ -22,26 +22,34 @@ const GridItem: GridComponents["Item"] = React.forwardRef(
       ref={ref}
       {...props}
       style={{ ...style }}
-      className=" w-full max-w-[420px] xl:max-w-[600px] aspect-square min-h-[350px]"
+      className=" min-w-full max-w-[420px] xl:max-w-[600px] aspect-square min-h-[350px]"
     >
       {children}
     </div>
   )
 );
+
 export const PlaylistPage = () => {
-  const [videos, setVideos] = useState<IVideo[]|[]>([]);
+  const [videos, setVideos] = useState<IVideo[] | []>([]);
+  const [playlistName, setPlaylistsName] = useState<string>("");
+  const [playlistDescription,setPlaylistDescription] = useState<string>("");
   const [searchParams] = useSearchParams();
   const playlistId = searchParams.get("playlistId") as string;
 
-  const { data, hasNextPage, fetchNextPage,isLoading,isSuccess} = useGetPlaylist({
-    playlistId,
-    limit: 5,
-  });
+  const { data, hasNextPage, fetchNextPage, isLoading, isSuccess } =
+    useGetPlaylist({
+      playlistId,
+      limit: 5,
+    });
   useEffect(() => {
     if (data && data.pages) {
       console.log("data.pages", data.pages);
 
-      const newVideos = data.pages.flatMap((page) => page.videos || []);
+      const newVideos = data.pages.flatMap((page) => {
+        setPlaylistsName(page.playlist.name);
+        setPlaylistDescription(page.playlist.description)
+        return page.playlist?.videos || [];
+      });
       console.log("new videos", newVideos);
 
       setVideos(newVideos);
@@ -50,30 +58,39 @@ export const PlaylistPage = () => {
 
   return (
     <>
-      <h1></h1>
-      <VirtuosoGrid
-        components={{ Item: GridItem, List: List }}
-        data={videos}
-        useWindowScroll
-        endReached={() => {
-          if (hasNextPage) {
-            fetchNextPage();
-          }
-        }}
-        isScrolling={(val) => console.log(val, "scrolling")}
-        itemContent={(_, data) => (
-          <VideoCard
-            isLoading={isLoading}
-            owner={data?.owner}
-            thumbnail={data.thumbnail}
-            videoId={data._id}
-            title={data.title}
-            viewsCount={data.views}
-            key={data._id}
-            isSuccess={isSuccess}
-            lazyLoading={false}
+      {videos.length > 0 ? (
+        <>
+          <h2>{playlistName}</h2>
+          <div>{playlistDescription}</div>
+          <VirtuosoGrid
+            components={{ Item: GridItem, List: List }}
+            style={{ height: "100%" }}
+            data={videos}
+            useWindowScroll
+            endReached={() => {
+              if (hasNextPage) {
+                fetchNextPage();
+              }
+            }}
+            isScrolling={(val) => console.log(val, "scrolling")}
+            itemContent={(_, data) => (
+              <VideoCardPlaylist
+                isLoading={isLoading}
+                owner={data.owner}
+                thumbnail={data.thumbnail}
+                videoId={data._id}
+                title={data.title}
+                viewsCount={data.views}
+                key={data._id}
+                isSuccess={isSuccess}
+                lazyLoading={false}
+              />
+            )}
           />
-        )}/>
+        </>
+      ) : (
+        <div>playlist is empty</div>
+      )}
     </>
   );
 };
