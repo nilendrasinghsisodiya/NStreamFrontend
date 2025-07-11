5fc0e46cda66172d78b8c05a7696743d8cbd5124
nilendrasinghsisodiya <niendrasinghsisodiya@gmail.com>
2025-07-11 21:31:24 +0530

    feat: added a videoCard to display in playlist

--- src/components/video/VideoCard.tsx
+++ src/components/video/VideoCard.tsx
@@ -70,7 +70,7 @@ const VideoCard = ({
                   videoTitle={title}
                   videoId={videoId}
                   className="flex flex-col w-full  z-40 px-5"
-                />{" "}
+                />
                 
                   <VideoOptions videoId={videoId} />
                
@@ -85,53 +85,79 @@ const VideoCard = ({
   );
 };
 
-/* will be avialabe after i create videoApi.ts with fetchVideo hooks  */
-type videoFetchProps = {
-  videoId: string;
+type videoCardPlaylistProps = {thumbnail: string;
+  title: string;
+  likeCount?: number;
+  viewsCount: number;
   style?: React.CSSProperties;
-  className?: string;
+  isLoading: boolean;
+  owner: Pick<IUser, "avatar" | "_id" | "username" | "subscriberCount">;
+  videoId: string;
+  isSuccess: boolean;
+  lazyLoading: boolean;
 };
 
-const VideoCardFetch = ({ videoId, style, className }: videoFetchProps) => {
-  const { data, isSuccess } = useGetVideo(videoId);
+export const VideoCardPlaylist = ({ thumbnail,
+  title,
+  style,
+  isLoading,
+  owner,
+  videoId,
+  lazyLoading,
+  isSuccess,
+}: videoCardPlaylistProps) => {
   const navigate = useNavigate();
+
   return (
-    <div
-      tabIndex={0}
-      style={style}
-      onClick={() => navigate(`/watch?videoId=${videoId}`)}
-      className={`flex flex-col cursor-pointer p-2 w-full max-h-full border-2 hover:scale-105 selection:border-foreground hover:shadow-[1px_1px_10px_rgba(23,23,255,0.5)] md:rounded-xl gap-2 my-5 ${className} `}
-    >
-      {isSuccess && data ? (
-        <div className="flex-1 aspect-auto ">
-          <img
-            srcSet={generateSrcSet(data.thumbnail)}
-            src={data.thumbnail}
-            alt={`videoId${data._id}thumbnail`}
-            className="aspect-video  md:rounded-2xl w-full"
-            loading="lazy"
-          />
-          <div className="flex flex-col">
-            <span className="text-xl text-foreground">{data.title}</span>
-            <div className="flex gap-20">
-              <span className="text-foreground text-xs">
-                {data.likesCount} likes
-              </span>
-              <span className="text-foreground text-xs">
-                {data.views} views
-              </span>
-            </div>
-          </div>
-        </div>
+    <>
+      {isLoading ? (
+        <VideoCardSkeleton />
       ) : (
-        <div>
-          <VideoCardSkeleton />
+        <div
+          tabIndex={0}
+          style={style}
+          onClick={() => {
+            navigate(`/watch?videoId=${videoId}`);
+          }}
+          className={`flex  cursor-pointer p-2 w-full max-h-full border-2 md:rounded-xl gap-2 my-5  `}
+        >
+          {isSuccess ? (
+            <>
+              <img
+                src={thumbnail}
+                alt={`${title}'s thumbnail`}
+                srcSet={generateSrcSet(thumbnail)}
+                className="aspect-video lg:rounded-3xl m-0.5 max-w-1/4 min-w-[200px] "
+                loading={lazyLoading ? "lazy" : "eager"}
+                width="100%"
+                height="100%"
+              />
+              <div className="flex w-full items-center justify-center">
+                <VideoAvatarStrip
+                  avatar={owner.avatar}
+                  subsCount={owner.subscriberCount}
+                  username={owner.username}
+                  videoTitle={title}
+                  videoId={videoId}
+                  className="flex flex-col w-full  z-40 px-5"
+                />
+                
+               
+              </div>
+            </>
+          ) : (
+            <span>something went wrong</span>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 };
+
+
+
+
+
 export const MemoVideoCard = memo(VideoCard);
-export { VideoCardFetch };
 
 export { VideoCard };
