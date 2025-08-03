e09cecd2f75380c9bf5010982a119b9c0871aad6
nilendrasinghsisodiya <niendrasinghsisodiya@gmail.com>
2025-08-03 20:21:10 +0530

    fix: made some changes to style added an icon

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,3 +1,4 @@
+import { RotateCcw } from "lucide-react";
 import { Component, ErrorInfo, ReactNode } from "react";
 
 interface ErrorBoundaryProps {
@@ -26,8 +27,9 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   render() {
     if (this.state.hasError) {
       return (
-        <div className="flex flex-col items-center justify-center h-screen text-center">
-          <h1 className="text-2xl font-bold text-red-500">Something went wrong.</h1>
+        <div className="flex flex-col items-center justify-center h-full w-full text-center">
+          <RotateCcw className="w-1/3 h-1/3 -z-30 text-accent relative"/>
+        <h1 className="text-2xl font-bold text-red-500/70">Something went wrong.</h1>
           <p className="text-gray-600">{this.state.error?.message || "An unexpected error occurred."}</p>
           <button 
             className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
