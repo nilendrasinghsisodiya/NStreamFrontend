528406ccf3080b52e3da0cb9b1831f318d971b52
nilendrasinghsisodiya <niendrasinghsisodiya@gmail.com>
2025-08-03 20:23:59 +0530

    fix: fixed minor bugs, changed style for better responsivness

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,21 +6,22 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 
 type Props = {
   children: React.ReactNode;
+  hideFooter?:boolean;
 };
 
-const Layout = ({ children }: Props) => {
+const Layout = ({ children, hideFooter}: Props) => {
   return (
     <div className="flex flex-row min-w-screen min-h-screen">
       <SidebarProvider defaultOpen={false} className="">
         <AppSidebar />
-        <div className="flex-1 flex flex-col min-h-screen items-center">
-          <Header />
-          <main className="h-full flex-col w-full max-w-screen  overflow-y-scroll items-center flex-1 ">
+        <div className="flex-1 flex flex-col min-h-screen items-center h-full w-full">
+         <div className="w-full h-22 z-50"><Header/></div> 
+          <main className="max-w-screen  w-full overflow-y-scroll items-center flex-1 p-3 m-auto">
             {children}
           </main>
-          <Suspense fallback={<p>loading</p>}>
+         {!hideFooter && <Suspense fallback={<p>loading</p>}>
             <MemoFooter key="footer" />
-          </Suspense>
+          </Suspense>}
         </div>
       </SidebarProvider>
     </div>
