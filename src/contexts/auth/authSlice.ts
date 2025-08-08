141c01d34f08011cd0bbd2ab0c77f0cee6b0c89e
nilendrasinghsisodiya <niendrasinghsisodiya@gmail.com>
2025-08-08 15:20:12 +0530

    add: added an auth state and fixed some bugs

--- src/contexts/auth/authSlice.ts
+++ src/contexts/auth/authSlice.ts
@@ -1,8 +1,10 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "@/ContextStore";
-
-const initialState: IUser = {
+import { useSelector } from "react-redux";
+export interface IAuthState extends IUser {isAuthenticated:boolean;}
+const initialState:IAuthState   =   {
   accessToken: "",
+  isAuthenticated:false,
   _id: "",
   username: "",
   avatar: "",
@@ -19,34 +21,45 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setUser: (state, user: PayloadAction<IUser>) => {
+    setUser: (state, user: PayloadAction<IAuthState>) => {
       Object.assign(state, user.payload);
     },
+    resetIsAuthenticated: (state)=>{
+      state.isAuthenticated = false;
 
+    },
+    setIsAuthenticated:(state)=>{state.isAuthenticated = true;},
     setRegistration: (
       state,
-      user: PayloadAction<{
+      action: PayloadAction<{
         _id: string;
         email: string;
         createdAt: string;
         accessToken?: string;
         username: string;
+        isAuthenticated:boolean;
       }>
     ) => {
-     return { ...state,accessToken : user.payload.accessToken,
-      _id: user.payload._id,
-      username : user.payload.username,
-      createdAt : user.payload.createdAt,
-      email :user.payload.email}
+    state.username = action.payload.username;
+    state._id = action.payload._id;
+    state.accessToken = action.payload.accessToken;
+    state.coverImage = action.payload.createdAt;
+    state.email = action.payload.email;
+    state.isAuthenticated = action.payload.isAuthenticated;
     },
-
     reset: (state) => {
       Object.assign(state, initialState);
     },
   },
 });
 
-export const { setUser, reset, setRegistration } = authSlice.actions;
+export const { setUser, reset, setRegistration ,resetIsAuthenticated,setIsAuthenticated} = authSlice.actions;
 
 export const selectUser = (state: RootState) => state.auth;
 export default authSlice.reducer;
+
+
+export const useIsAuthenticated = ()=>{
+  const {isAuthenticated} = useSelector(selectUser);
+  return isAuthenticated;
+}
