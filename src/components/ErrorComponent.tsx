107580fd9ca7326384488e4587ac6ed44e5e2b2c
nilendrasinghsisodiya <niendrasinghsisodiya@gmail.com>
2025-08-08 15:13:45 +0530

    fix: changed a bit of style and text alignment

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,17 +1,19 @@
 type errorScreenProps = {
   mainMessage: string;
   secondaryMessage?: string;
+  color?:string;
 };
 export const ErrorScreen = ({
   mainMessage,
+  color,
   secondaryMessage,
 }: errorScreenProps) => {
   return (
-    <div>
-      <span className="text-2xl text-accent text-center tracking-wide">
+    <div className="flex flex-col w-full h-full justify-center items-center">
+      <span color={color} className="text-xl sm:text-2xl md:text-3xl lg:text-4xl capitalize text-foreground/50 text-center tracking-wide">
         {mainMessage}
       </span>
-      <span className="text-xl text-accent/90 tracking-wide text-center">
+      <span color={color} className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-foreground/30 capitalize tracking-wide text-center">
         {secondaryMessage || ""}
       </span>
     </div>
