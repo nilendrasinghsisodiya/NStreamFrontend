1d3b0792a256e8c5307a7a60b56c02b4ec57a40b
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2026-01-10 00:08:27 +0530

    fix: fixed wrong import for BrowserRouter and typo in toaster id

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import { QueryClientProvider as QueryProvider } from "@tanstack/react-query";
 import { AppRoutes } from "./AppRoutes";
@@ -10,16 +10,27 @@ import { queryClient } from "./api/ApiClient";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PersistGate } from "redux-persist/integration/react";
 import ErrorBoundary from "./components/ErrorBoundary";
-import {Toaster} from "sonner"
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate persistor={presister} loading={null}>
         <QueryProvider client={queryClient}>
-          <Toaster position="bottom-center" richColors closeButton theme="system" duration={6000} id="gloabal" />
-          <Toaster offset={{left:"50vw",top:"50vh"}}  duration={Infinity} id="confirmation-center" />
-            <ReactQueryDevtools client={queryClient} />
+          <Toaster
+            position="bottom-center"
+            richColors
+            id="global"
+            closeButton
+            theme="system"
+            duration={6000}
+          />
+          <Toaster
+            offset={{ left: "50vw", top: "50vh" }}
+            id="conformation"
+            duration={Infinity}
+          />
+          <ReactQueryDevtools client={queryClient} />
           <ErrorBoundary>
             <Router>
               <AppRoutes />
@@ -28,5 +39,5 @@ createRoot(document.getElementById("root")!).render(
         </QueryProvider>
       </PersistGate>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
