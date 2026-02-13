631ff3b1990fcc33c8327f57e4275e93003a11ee
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2026-02-13 23:12:54 +0530

    feat: hls and video hooks for handling video player dependencies

--- src/hooks/useHls.ts
+++ src/hooks/useHls.ts
@@ -3,80 +3,77 @@ import Hls from "hls.js";
 import { convertToHLS } from "@/utils";
 
 type props = {
-  videoUrl: string;
-  videoRef: React.RefObject<HTMLVideoElement>;
+	videoUrl: string;
 };
 type HlsObject = {
-  qualities: Quality[];
-  totalDuration: number;
-  hlsRef: React.RefObject<Hls>;
+	qualities: Quality[];
+	totalDuration: number;
+	hlsRef: React.RefObject<Hls>;
 };
-export type Quality =
-  | "360p"
-  | "480p"
-  | "720p"
-  | "1080p"
-  | "1440p"
-  | "240p"
-  | "2160p";
+ type Qualities =
+	"360p"
+	| "480p"
+	| "720p"
+	| "1080p"
+	| "1440p"
+	| "240p"
+	| "2160p";
+export type Quality = {q: Qualities; index:number;}	
 
-export function useHls({ videoUrl, videoRef }: props): HlsObject {
-  const [qualities, setQualities] = useState<Quality[]>([]);
-  const hlsRef = useRef<Hls | null>(null);
-  const [totalDuration, setTotalDuration] = useState<number>(0);
-  useEffect(() => {
-    if (!Hls.isSupported()) return;
-    if (hlsRef && hlsRef.current) return;
-    if (!videoRef && !videoRef.current) return;
-    hlsRef.current = new Hls();
-    const hlsSrc = convertToHLS(videoUrl);
-    const hls = hlsRef.current;
-    const video = videoRef.current;
-    hls.loadSource(hlsSrc);
-    hls.attachMedia(video);
-    const quals: Quality[] = [];
-    hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
-      const levels = data.levels;
-      console.log("video levels", levels);
-      levels.map((level) => {
-        switch (level.width) {
-          case 1080:
-            quals.push("1080p");
-            break;
-          case 720:
-            quals.push("720p");
-            break;
-          case 360:
-            quals.push("360p");
-            break;
-          case 480:
-            quals.push("480p");
-            break;
-          case 240:
-            quals.push("240p");
-            break;
-          case 1440:
-            quals.push("1440p");
-            break;
-          case 2160:
-            quals.push("2160p");
-            break;
-          default:
-            break;
-        }
-        setQualities(quals);
-      });
-      hls.on(Hls.Events.LEVEL_LOADED, (_, data) => {
-        if (data.details) {
-          setTotalDuration(data.details.totalduration);
-        }
-      });
-      hls.on(Hls.Events.ERROR, (_, error) => {
-        console.error("hls error:: ", error);
-      });
-    });
+export function useHls({ videoUrl}: props): HlsObject {
+	const [qualities, setQualities] = useState<Quality[]>([]);
+	const hlsRef = useRef<Hls | null>(null);
+	const [totalDuration, setTotalDuration] = useState<number>(0);
+	useEffect(() => {
+		if (!Hls.isSupported()) return;
+		if (hlsRef && hlsRef.current) return;
+		hlsRef.current = new Hls();
+		const hlsSrc = convertToHLS(videoUrl);
+		const hls = hlsRef.current;
+		hls.loadSource(hlsSrc);
+		const quals: Quality[] = [];
+		hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
+			const levels = data.levels;
+			console.log("video levels", levels);
+			levels.map((level,index) => {
+				switch (level.width) {
+					case 1080:
+						quals.push({q:"1080p",index});
+						break;
+					case 720:
+						quals.push({q:"720p",index});
+						break;
+					case 360:
+						quals.push({q:"360p",index});
+						break;
+					case 480:
+						quals.push({q:"480p",index});
+						break;
+					case 240:
+						quals.push({q:"240p",index});
+						break;
+					case 1440:
+						quals.push({q:"1440p",index});
+						break;
+					case 2160:
+						quals.push({q:"2160p",index});
+						break;
+					default:
+						break;
+				}
+				setQualities(quals);
+			});
+			hls.on(Hls.Events.LEVEL_LOADED, (_, data) => {
+				if (data.details) {
+					setTotalDuration(data.details.totalduration);
+				}
+			});
+			hls.on(Hls.Events.ERROR, (_, error) => {
+				console.error("hls error:: ", error);
+			});
+		});
 
-    return () => hls.destroy();
-  }, [videoUrl, videoRef]);
-  return { qualities, totalDuration, hlsRef };
+		return () => hls.destroy();
+	}, [videoUrl, videoRef]);
+	return { qualities, totalDuration, hlsRef };
 }
--- src/hooks/useVideo.ts
+++ src/hooks/useVideo.ts
@@ -1,82 +1,151 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback, useMemo, useEffect } from "react";
 import { useHls } from "@/hooks/useHls";
 import type { Quality } from "@/hooks/useHls";
+import { useThrottle } from "./useThrottle";
 type props = {
-  videoUrl: string;
-  videoRef: React.RefObject<HTMLVideoElement>;
+	videoUrl: string;
+	videoRef: React.RefObject<HTMLVideoElement>;
 };
 export function useVideo({ videoUrl, videoRef }: props) {
-  // hooks
-  const { qualities, hlsRef, totalDuration } = useHls({ videoUrl, videoRef });
-  // states
-  const [quality, setQuality] = useState<Quality>("480p");
-  const [ps, setPs] = useState<number>(1.0);
-  const [curTime, setCurTime] = useState<number>(0);
-  const [vol, setVol] = useState<number>(0.5);
-  const [isMute, setIsMute] = useState<boolean>(false);
-  const [isPlaying, setIsPlaying] = useState<boolean>(true);
-  const [controls, setControls] = useState<boolean>(true);
+	// hooks
+	const { qualities, hlsRef, totalDuration } = useHls({ videoUrl });
+	// states
+	const [qual, setQual] = useState<Quality>(qualities[hlsRef.current.currentLevel]);
+	const [ps, setPs] = useState<number>(1.0);
+	const [curTime, setCurTime] = useState<number>(0);
+	const [vol, setVol] = useState<number>(0.5);
+	const [isMute, setIsMute] = useState<boolean>(false);
+	const [isPlaying, setIsPlaying] = useState<boolean>(true);
+	const [controls, setControls] = useState<boolean>(true);
 
-  // utility functions
-  const setPlay = useCallback((b: boolean) => {}, []);
-  const togglePlayPause = useCallback(() => {}, []);
-  const setQual = useCallback((q: Quality) => {}, []);
-  const setVolume = useCallback((n: number) => {}, []);
-  const setCurrentTime = useCallback((n: number) => {}, []);
-  const setPlaybackSpeed = useCallback((n: number) => {}, []);
-  const toggleControls = useCallback(() => {}, []);
-  const toggleMute = useCallback(() => {}, []);
-  const toggleFullScreen = useCallback(() => {}, []);
-  const api = useMemo(() => {
-    return {
-      // togglers
-      toggleControls,
-      togglePlayPause,
-      toggleMute,
-      toggleFullScreen,
 
-      // setters
-      setPlay,
-      setQual,
-      setVolume,
-      setCurrentTime,
-      setPlaybackSpeed,
+	const sct = () => {
+		if (videoRef && videoRef.current) {
+			setCurTime(videoRef.current.currentTime);
+		}
+	}
+	const throttledCurrentTime = useThrottle(sct, 200);
 
-      // state
-      volume: vol,
-      playbackSpeed: ps,
-      currentTime: curTime,
-      isPlaying,
-      controls,
-      quality,
-      isMute,
-      qualities,
-      totalDuration,
-    };
-  }, [
-    // setters
-    setPlay,
-    setVolume,
-    setCurrentTime,
-    setQuality,
-    setPlaybackSpeed,
-    toggleFullScreen,
+	// utility functions
+	const setPlay = useCallback((b: boolean) => {
+		const video = videoRef.current;
+		if (b) {
+			video?.play();
+		}
+		else {
+			video?.pause();
+		}
+	}, [videoRef]);
+	const togglePlayPause = useCallback(() => {
+		const video = videoRef.current;
+		if (isPlaying) {
+			video?.pause();
 
-    // togglers
-    toggleControls,
-    togglePlayPause,
-    toggleMute,
+		} else {
+			video?.play();
+		}
 
-    // states
-    vol,
-    ps,
-    curTime,
-    isPlaying,
-    isMute,
-    quality,
-    qualities,
-    totalDuration,
-  ]);
+	}, [videoRef]);
+	const setQuality = useCallback((q: Quality) => {
+		const hls = hlsRef.current;
+		const qNum = q.index;
+		hls.currentLevel = qNum;
+		setQual(q);
+	}, [hlsRef]);
+	const setVolume = useCallback((n: number) => {
+		const video = videoRef.current;
+		video.volume = n;
+	}, [videoRef]);
+	const setPlaybackSpeed = useCallback((n: number) => {
+		const video = videoRef.current;
+		video.playbackRate = n;
+	}, [videoRef]);
+	const toggleControls = useCallback(() => {
+		setControls((prev) => !prev);
+	}, []);
+	const toggleMute = useCallback(() => {
+		const video = videoRef.current;
+		video.muted = !video.muted;
+	}, [videoRef]);
+	const toggleFullScreen = useCallback(() => {
+		videoRef.current.requestFullscreen();
+	}, [videoRef]);
+	const setSeek = useCallback((n: number) => {
+		videoRef.current.currentTime = n;
+	}, [videoRef]);
 
-  return api;
+
+	// effects
+
+	// state sync
+	useEffect(() => {
+		const video = videoRef.current;
+
+		// handlers
+		const currentTimeHandler = () => { throttledCurrentTime(); }
+		const volHandler = () => {
+			setVol(video.volume);
+			setIsMute(video.muted)
+		}
+		const psHandler = () => {
+			setPs(video.playbackRate)
+		}
+		const playHandler = () => {
+			setIsPlaying(true)
+		}
+		const pauseHandler = () => { setIsPlaying(false); }
+
+		// event listeners
+		video.addEventListener("timeupdate", currentTimeHandler);
+		video.addEventListener("play", playHandler);
+		video.addEventListener("pause", pauseHandler)
+		video.addEventListener("volumechange", volHandler)
+		video.addEventListener("ratechangee", psHandler);
+
+		// clean up
+		return () => {
+			video.removeEventListener("timeupdate", currentTimeHandler);
+			video.removeEventListener("play", playHandler);
+			video.removeEventListener("pause", pauseHandler);
+			video.removeEventListener("ratechange", psHandler);
+			video.removeEventListener("volumechange", volHandler);
+		}
+	}, [videoRef])
+
+	// load video source
+	useEffect(() => {
+		if (hlsRef && hlsRef.current) {
+			if (videoRef && videoRef.current) {
+				const hls = hlsRef.current;
+				hls.attachMedia(videoRef.current);
+			}
+		}
+
+	}, [hlsRef, videoRef])
+
+	return {
+		// togglers
+		toggleControls,
+		togglePlayPause,
+		toggleMute,
+		toggleFullScreen,
+
+		// setters
+		setPlay,
+		setQuality,
+		setVolume,
+		setSeek,
+		setPlaybackSpeed,
+
+		// state
+		volume: vol,
+		playbackSpeed: ps,
+		currentTime: curTime,
+		isPlaying,
+		controls,
+		quality: qual,
+		isMute,
+		qualities,
+		totalDuration,
+	}
 }
