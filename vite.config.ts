8b1335a484acddcd35f9aab034512e00251a823c
nilendrasinghsisodiya <nilendrasinghsisodiya@gmail.com>
2026-02-14 12:49:57 +0530

    added proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,19 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+				secure: false,
+			}
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
